1b92b5f411433ca8cefad056b7f9ac1c65bc537c
SashkaHacker <am942081@gmail.com>
2023-11-02 22:29:28 +0300

    add files

---
+++ ex1.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Вариант 11
+
+if __name__ == "__main__":
+    A = [int(input()) for _ in range(10)]
+    count = []
+    for i in A:
+        if i < 0 and i % 7 == 0:
+            count.append(i)
+
+    print(f'Количество элементов: {len(count)}. Сумма элементов: {sum(count)}.')
---
+++ ex2.py
@@ -0,0 +1,7 @@
+#!usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Вариант 11
+
+if __name__ == '__main__':
+    pass
