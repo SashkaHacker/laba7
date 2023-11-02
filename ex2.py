170527a21d2086707859f245b8c12df00a92b4d3
SashkaHacker <am942081@gmail.com>
2023-11-02 23:32:00 +0300

    add ex2

--- ex2.py
+++ ex2.py
@@ -4,4 +4,29 @@
 # Вариант 11
 
 if __name__ == '__main__':
-    pass
+    lst = [float(input("Введите элемент списка: "))
+           for i in range(int(input("Введите длину списка: ")))]
+    # решение 1-ой задачи
+    maxx = lst[1]
+    for i in lst:
+        if abs(i) > abs(maxx):
+            maxx = i
+    print(f"Номер максимального по модулю элемента списка: "
+          f"{lst.index(maxx) + 1}")
+
+    # решение 2-ой задачи
+    summ = 0
+    flag = False
+    for i in lst:
+        if flag:
+            summ += i
+        elif i >= 0:
+            flag = True
+    print(f"Сумма элементов списка, расположенных после 1-го"
+          f" положительного числа: {summ}")
+
+    # преобразование списка
+    lst = [3.0, 2.0, 9.0, 10.5, 11.9]
+    a, b = int(input("Введите а: ")), int(input("Введите b: "))
+    new_lst = sorted(lst, key=lambda x: int(x) >= a <= b, reverse=True)
+    print(new_lst)
